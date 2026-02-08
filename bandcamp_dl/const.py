c20855c2162fcce9142bbd5ad69f49eab031f692
AndBoyS <andboyxd@gmail.com>
2026-02-08 19:22:22 +0300

    extract version when installed

--- bandcamp_dl/const.py
+++ bandcamp_dl/const.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import importlib.metadata
 from pathlib import Path
 from typing import Any
 
@@ -7,5 +8,9 @@ import toml
 
 REPO_DIR = Path(__file__).parents[1]
 
-metadata: dict[str, Any] = toml.load(REPO_DIR / "pyproject.toml")
-VERSION: str = metadata["project"]["version"]
+pyproject_path = REPO_DIR / "pyproject.toml"
+if pyproject_path.exists():
+    metadata: dict[str, Any] = toml.load(pyproject_path)
+    VERSION: str = metadata["project"]["version"]
+else:
+    VERSION = importlib.metadata.version("bandcamp-downloader")
