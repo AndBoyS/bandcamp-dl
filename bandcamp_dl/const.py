3b8251171bf39a30c64ed81e4f570f6113069c5d
AndBoyS <andboyxd@gmail.com>
2026-02-08 19:14:00 +0300

    dont depend on git when running script

--- bandcamp_dl/const.py
+++ bandcamp_dl/const.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
-import subprocess
 from pathlib import Path
 from typing import Any
 
 import toml
 
-REPO_DIR = Path(subprocess.check_output(["git", "rev-parse", "--show-toplevel"]).decode().strip())
+REPO_DIR = Path(__file__).parents[1]
 
 metadata: dict[str, Any] = toml.load(REPO_DIR / "pyproject.toml")
 VERSION: str = metadata["project"]["version"]
